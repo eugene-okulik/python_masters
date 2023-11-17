a465c764dfc741b5a9a46c088d6d7c1416baba2a
Natallia <natatyusha@mail.ru>
2023-11-17 21:52:17 +0300

    homework3_done

---
+++ homework/natallia_tsiushkevich/homework_3/task3.py
@@ -0,0 +1,7 @@
+a = 2
+b = 2
+print((a + b) / 2)
+print((a * b) ** 0.5)
+# загуглила еще такую переменную найти корень
+from math import sqrt
+print(sqrt(a + b))
---
+++ homework/natallia_tsiushkevich/homework_3/task4.py
@@ -0,0 +1,4 @@
+katet1 = 2
+katet2 = 3
+print((katet1 * katet1) + (katet2 * katet2))
+print((katet1 * katet2) * 2)
