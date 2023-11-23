dd0e3bb8a40a4273ed6a9d892419e10c79c3c28e
Elizaveta Buglak <qalizabuglak@gmail.com>
2023-11-23 22:48:09 +0100

    fixed problem with flake8

--- homework/elizaveta_buglak/homework_4/program for homework_4.py
+++ homework/elizaveta_buglak/homework_4/program for homework_4.py
@@ -1,16 +1,3 @@
-# Напишите программу, которая спрашивает:
-
-# как вас зовут?
-# что вы хотите купить?
-# сколько у вас есть?
-# сколько можете отложить в месяц?
-
-# В результате программа должна распечатать, например, такое:
-
-# Привет, Петя. На покупку Ferrari тебе не хватает 1000000
-# Возможность совершения покупки: False
-# До покупки осталось 200 месяцев
-
 import sys
 cost_purchase = 1000000
 amount_save = 5000
@@ -23,8 +10,8 @@ purchase = input('What do you want to buy?')
 if purchase == 'Ferrari':
     print('Ok, let\'s analyze your possibilities!')
 else:
-    print('My program doesn\'t understand you')
-    sys.exit()
+    print('My program does\'t understand you')
+    sys.exit(0)
 
 amount_money = float(input('How much money do you have?'))
 if cost_purchase == amount_money:
