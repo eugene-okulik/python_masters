df249ecfa00877d86d61fb3a9186e505cfdeec57
Elizaveta Buglak <qalizabuglak@gmail.com>
2023-11-23 18:45:35 +0100

    updated the format of mathematical operations in code

--- homework/elizaveta_buglak/homework_3/task_3.py
+++ homework/elizaveta_buglak/homework_3/task_3.py
@@ -3,5 +3,5 @@ import math
 a = 6
 b = 10
 
-print('arithmetic average=', (a + b)/2)
+print('arithmetic average=', (a + b) / 2)
 print('geometric average=', (math.sqrt(a * b)))
--- homework/elizaveta_buglak/homework_3/task_4.py
+++ homework/elizaveta_buglak/homework_3/task_4.py
@@ -2,7 +2,7 @@ import math
 
 a = 2
 b = 3
-c = (math.sqrt((a**2) + (b**2)))
+c = (math.sqrt((a ** 2) + (b ** 2)))
 print("Hypotenuse equals to:", c)
 s = 1/2 * (a * b)
 print("The area of the triangle will be:", s)
