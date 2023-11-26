941b4a4997e7b699007630d1baa1df1dad3c740f
Dmitry Samokhin <virys2@gmail.com>
2023-11-26 20:35:13 +0300

    решил задачки для уроков 6 и 7, так же пофиксил ошибку на внимательность урока 5

---
+++ homework/dmitry_samokhin/lesson_6/exercise_3.py
@@ -0,0 +1,8 @@
+students = ["Ivanov", "Petrov", "Sidorov"]
+
+subjects = ["math", "biology", "geography"]
+
+print(
+    f"Students {students[0]}, {students[1]}, {students[2]} study these subjects: "
+    f"{subjects[0]}, {subjects[1]}, {subjects[2]}"
+)
