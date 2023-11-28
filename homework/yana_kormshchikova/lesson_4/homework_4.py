2fe03468eefddf6c007862f8f4d9b5b4a3535cc4
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-11-28 15:18:10 +0300

    homework_4 done

--- homework/yana_kormshchikova/lesson_4/homework_4.py
+++ homework/yana_kormshchikova/lesson_4/homework_4.py
@@ -1,3 +1,18 @@
+# Напишите программу, которая спрашивает:
+#
+# как вас зовут?
+# что вы хотите купить?
+# сколько это стоит?
+# сколько у вас есть?
+# сколько можете отложить в месяц?
+# В результате программа должна распечатать, например, такое:
+#
+# Привет, Петя. На покупку Ferrari тебе не хватает 1000000
+#
+# Возможность совершения покупки: False
+#
+# До покупки осталось 200 месяцев
+
 name = input('What is your name? ')
 items = input('What do you want to buy? ')
 price = int(input('How much does it cost? '))
@@ -14,4 +29,3 @@ else:
           f"For now you can't buy {items}. "
           f"You need {price - savings} more. "
           f"You need to save for another {((price - savings) // monthly_save)} months to buy")
-
