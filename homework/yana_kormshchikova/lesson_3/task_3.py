2f7bbcb162618dc83570fad152846ffa7ed195dd
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-11-29 22:20:12 +0300

    formatting fixed

--- homework/yana_kormshchikova/lesson_3/task_3.py
+++ homework/yana_kormshchikova/lesson_3/task_3.py
@@ -5,8 +5,8 @@ import math
 a = 4
 b = 13
 
-avr = (a+b)/2
-geometr = math.sqrt(a*b)
+avr = (a + b) / 2
+geometr = math.sqrt(a * b)
 
 print(f'Среднее арифметическое чисел {a} и {b} - {avr}')
 print(f'Среднее геометричесое чисел = {geometr}')
