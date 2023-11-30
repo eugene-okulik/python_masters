b885e5756e4c83fefaf4989c29c53966d786d768
Kobzeva Alina <kobzeva_lina@mail.ru>
2023-11-30 23:46:54 +0300

    homework 1 and 3 tasks

---
+++ homework/kobzeva_alina/homework_6/lesson_6.py
@@ -0,0 +1,27 @@
+# Задание 1
+
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+
+name = person[0]
+last_name = person[1]
+city = person[2]
+phone = person[3]
+country = person[4]
+
+print(f'My name is {name} {last_name}. My phone number is {phone}. I live in {city}, {country}.')
+
+# Задание 2
+
+# str1 = 'результат операции: 42'
+# str2 = 'результат работы программы: 547'
+# str3 = 'результат: 5'
+
+
+
+# Задание 3
+
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+print(f'Students {",".join(students)} study these subjects: {",".join(subjects)}')
+
