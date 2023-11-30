d451129945bed23a6a2aaa495a225b0c58662253
Elizaveta Buglak <qalizabuglak@gmail.com>
2023-11-30 22:15:11 +0100

    homework_6 done

---
+++ homework/elizaveta_buglak/homework_5/task for homework_5.py
@@ -0,0 +1,33 @@
+my_dict = {
+    'tupple': (1, 5, 10, 'hello', True),
+    'list': [2, 4, 6, 'Liza', False],
+    'dict': {'number': '22', 'name': 'Liza', 'surname': 'Buglak', 'age': '22', 'Belarus': 'True'},
+    'set': {3, 6, 9, 'hello', 'how are you?'}
+}
+# Действия с элементами словаря my_dict:
+
+# 1: Для того, что хранится под ключом ‘tuple’:
+# выведите на экран последний элемент
+print(my_dict['tupple'][-1])
+
+# 2: Для того, что хранится под ключом ‘list’:
+# добавьте в конец списка еще один элемент
+# удалите второй элемент списка
+my_dict['list'].append('Hello everyone')
+my_dict['list'].pop(1)
+
+# 3: Для того, что хранится под ключом ‘dict’:
+# добавьте элемент с ключом ('i am a tuple',) и любым значением
+# удалите какой-нибудь элемент
+my_dict['dict']['i am a tuple'] = 'new value was added'
+my_dict['dict'].pop('surname')
+
+# 4: Для того, что хранится под ключом ‘set’:
+# добавьте новый элемент в множество
+# удалите элемент из множества
+my_dict['set'].add('True')
+my_dict['set'].remove(9)
+print(my_dict['set'])
+
+# В конце выведите на экран весь словарь
+print(my_dict)
---
+++ homework/elizaveta_buglak/homework_6/task 1 for homework_6.py
@@ -0,0 +1,4 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+print(f'Hello, my name is {name} {last_name}. I live in {city}. My phone number is {phone}. '
+      f'I really like my country: {country}')
