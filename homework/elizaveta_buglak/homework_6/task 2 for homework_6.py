2acb0f171786adf3e26b3d9a77246e58761114d7
Elizaveta Buglak <qalizabuglak@gmail.com>
2023-12-01 15:11:35 +0100

    fixed task 2 in homework

--- homework/elizaveta_buglak/homework_6/task 2 for homework_6.py
+++ homework/elizaveta_buglak/homework_6/task 2 for homework_6.py
@@ -2,6 +2,14 @@ str1 = 'результат операции: 42'
 str2 = 'результат работы программы: 547'
 str3 = 'результат: 5'
 
-print(int((str1[20:])) + 10)
-print(int(str2[27:]) + 10)
-print(int(str3[11:]) + 10)
+str1_index = str1.find('42')
+str1_result = int(str1[str1_index:])
+print(str1_result + 10)
+
+str2_index = str2.find('547')
+str2_result = int(str2[str2_index:])
+print(str2_result + 10)
+
+str3_index = str3.find('5')
+str3_result = int(str3[str3_index:])
+print(str3_result + 10)
