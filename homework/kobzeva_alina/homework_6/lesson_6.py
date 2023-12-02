a5cc03298f520d9104aabe9e097fc8dcd32ddb80
Kobzeva Alina <kobzeva_lina@mail.ru>
2023-12-02 23:06:35 +0300

    fix index

--- homework/kobzeva_alina/homework_6/lesson_6.py
+++ homework/kobzeva_alina/homework_6/lesson_6.py
@@ -6,13 +6,13 @@ name, last_name, city, phone, country = person
 
 # Задание 2
 
-str1 = 'результат операции: 42'
-str2 = 'результат работы программы: 547'
-str3 = 'результат: 5'
+str_1 = 'результат операции: 42'
+str_2 = 'результат работы программы: 547'
+str_3 = 'результат: 5'
 
-print(int((str1[20:])) + 10)
-print(int(str2[27:]) + 10)
-print(int(str3[11:]) + 10)
+print(int(str_1[str_1.index(':') + 2:]) + 10)
+print(int(str_2[str_2.index(':') + 2:]) + 10)
+print(int(str_3[str_3.index(':') + 2:]) + 10)
 
 
 # Задание 3
