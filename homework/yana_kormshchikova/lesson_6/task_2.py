2b36a35b997b571057519eeaedb70ae204f0248d
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-12-03 17:43:12 +0300

    fixed formatting

--- homework/yana_kormshchikova/lesson_6/task_2.py
+++ homework/yana_kormshchikova/lesson_6/task_2.py
@@ -3,12 +3,11 @@ line_2 = 'результат работы программы: 547'
 line_3 = 'результат: 5'
 
 line_1_index = line_1.find(':')
-num1 = int(line_1[line_1_index + 1:])+10
+num1 = int(line_1[line_1_index + 1:]) + 10
 line_2_index = line_2.find(':')
-num2 = int(line_2[line_2_index + 1:])+10
+num2 = int(line_2[line_2_index + 1:]) + 10
 line_3_index = line_3.find(':')
-num3 = int(line_3[line_3_index + 1:])+10
-
+num3 = int(line_3[line_3_index + 1:]) + 10
 
 print(num1)
 print(num2)
