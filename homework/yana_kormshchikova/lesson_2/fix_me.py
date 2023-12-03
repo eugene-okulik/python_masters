4f5044466783869c2497416fa13185c13a0c6b65
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-12-03 17:14:43 +0300

    added changes after review

--- homework/yana_kormshchikova/lesson_2/fix_me.py
+++ homework/yana_kormshchikova/lesson_2/fix_me.py
@@ -3,7 +3,7 @@ import random
 
 
 def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
-    "A Binary Search Example which has O(log n) time complexity."
+    """A Binary Search Example which has O(log n) time complexity."""
     if lb <= ub:
         mid: int = lb + (ub - lb) // 2
         if arr[mid] == target:
@@ -20,6 +20,11 @@ def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
 
 if __name__ == '__main__':
     rand_num_li: List[int] = sorted([random.randint(1, 50) for _ in range(10)])
-target: int = random.randint(1, 50)
-print("List: {}\nTarget: {}\nIndex: {}".format(rand_num_li, target,
-                                               binary_search(rand_num_li, 0, len(rand_num_li) - 1, target)))
+    target: int = random.randint(1, 50)
+    print("List: {}\nTarget: {}\nIndex: {}".format(
+        rand_num_li,
+        target,
+        binary_search(
+            rand_num_li, 0, len(rand_num_li) - 1, target)
+        )
+    )
