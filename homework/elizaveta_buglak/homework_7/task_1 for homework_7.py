5b57b3c6edc81fb9f48a16ccd36c2c07b08183da
Elizaveta Buglak <qalizabuglak@gmail.com>
2023-12-04 22:57:43 +0100

    homework_7 done

---
+++ homework/elizaveta_buglak/homework_7/task_1 for homework_7.py
@@ -0,0 +1,15 @@
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
+        'dignissim vitae libero')
+text_1 = text.split(' ')
+
+text_final = []
+
+for word in text_1:
+    if word.endswith(','):
+        text_final.append(f"{word[:-1] + 'ing' + word[-1:]}")
+    elif word.endswith('.'):
+        text_final.append(f"{word[:-1] + 'ing' + word[-1:]}")
+    else:
+        text_final.append(word + 'ing')
+
+print(' '.join(text_final))
---
+++ homework/elizaveta_buglak/homework_7/task_2 for homework_7.py
@@ -0,0 +1,9 @@
+for number in range(1,100):
+    if number % 3 == 0:
+        print ('Fuzz')
+    elif number % 5 == 0:
+        print('Buzz')
+    elif number % 5 == 0 and number % 3 == 0:
+        print('FuzzBuzz')
+    else:
+        print(number)
