8f76c653a1a503889a28750306ed7cc29a820787
lzbglk123 <144231595+lzbglk123@users.noreply.github.com>
2023-12-04 23:07:52 +0100

    Update task_2 for homework_7.py

--- homework/elizaveta_buglak/homework_7/task_2 for homework_7.py
+++ homework/elizaveta_buglak/homework_7/task_2 for homework_7.py
@@ -1,10 +1,10 @@
-for number in range(1, 100):
+for number in range(1, 101):
     if number % 3 == 0:
         print('Fuzz')
     elif number % 5 == 0:
         print('Buzz')
     elif number % 5 == 0 and number % 3 == 0:
-        print('FuzzBuzz')
+        print('Fuzzbuzz')
     else:
         print(number)
         
