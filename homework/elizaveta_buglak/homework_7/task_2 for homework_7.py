1959d1175055023c74914fcc1a9c51c9142e02e5
Elizaveta Buglak <qalizabuglak@gmail.com>
2023-12-04 23:03:36 +0100

    fixed pylint problem

--- homework/elizaveta_buglak/homework_7/task_2 for homework_7.py
+++ homework/elizaveta_buglak/homework_7/task_2 for homework_7.py
@@ -1,6 +1,6 @@
-for number in range(1,100):
+for number in range(1, 100):
     if number % 3 == 0:
-        print ('Fuzz')
+        print('Fuzz')
     elif number % 5 == 0:
         print('Buzz')
     elif number % 5 == 0 and number % 3 == 0:
