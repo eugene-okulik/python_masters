36a27c5680f41b0a3a9953170670f2990c4198dd
lzbglk123 <144231595+lzbglk123@users.noreply.github.com>
2023-12-04 23:10:48 +0100

    Update task_1 for homework_7.py

--- homework/elizaveta_buglak/homework_7/task_1 for homework_7.py
+++ homework/elizaveta_buglak/homework_7/task_1 for homework_7.py
@@ -1,9 +1,8 @@
-text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
-        'dignissim vitae libero')
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
+        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 text_1 = text.split(' ')
 
 text_final = []
-
 for word in text_1:
     if word.endswith(','):
         text_final.append(f"{word[:-1] + 'ing' + word[-1:]}")
