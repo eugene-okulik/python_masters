30b7f9c6ec2579349d4ce8f6fcaac91b02c2fab0
Kobzeva Alina <kobzeva_lina@mail.ru>
2023-12-05 23:12:50 +0300

    fix task3

--- homework/kobzeva_alina/homework_8/lesson_8 (task3).py
+++ homework/kobzeva_alina/homework_8/lesson_8 (task3).py
@@ -3,6 +3,7 @@ def get_result(str):
     result = int(str[index:]) + 10
     return result
 
+
 str_1 = 'результат операции: 42'
 str_2 = 'результат работы программы: 547'
 str_3 = 'результат: 5'
@@ -10,4 +11,3 @@ str_3 = 'результат: 5'
 print(get_result(str_1))
 print(get_result(str_2))
 print(get_result(str_3))
-
