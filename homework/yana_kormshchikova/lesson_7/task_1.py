50d06d67a816707f5bcb4be5826decfccb1d9874
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-12-06 22:44:01 +0300

    homework 7 done

---
+++ homework/yana_kormshchikova/lesson_7/task_1.py
@@ -0,0 +1,18 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
+        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+
+new_text = text.split()
+converted_words = []
+
+for word in new_text:
+    if ',' in word:
+        new_word = word[0:-1] + 'ing' + word[-1:]
+        converted_words.append(new_word)
+    elif '.' in word:
+        new_word = word[0:-1] + 'ing' + word[-1:]
+        converted_words.append(new_word)
+    else:
+        new_word = word + 'ing'
+        converted_words.append(new_word)
+
+print(' '.join(converted_words))
