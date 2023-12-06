c4da72351b8650d36f72058187ad4dc94c2493c4
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-12-06 22:48:52 +0300

    format fixing

--- homework/yana_kormshchikova/lesson_7/task_1.py
+++ homework/yana_kormshchikova/lesson_7/task_1.py
@@ -1,5 +1,5 @@
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. ' \
-        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, ' \
+       'dignissim vitae libero'
 
 new_text = text.split()
 converted_words = []
