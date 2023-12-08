c86ba3d2f3cee1c58969f3f6f7423af70e593b2c
EG-QA-12 <egskypro@mail.ru>
2023-12-08 13:37:04 +0300

    Homework2 ready

--- homework/evgeny_gusinets/homework_2/Homework_2.py
+++ homework/evgeny_gusinets/homework_2/Homework_2.py
@@ -1,24 +1,19 @@
 from typing import List
 import random
 
-
-def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
-    "A Binary Search Example which has O(log n) time complexity."
-    if lb <= ub:
-        mid = lb + (ub - lb) // 2
+def binary_search(arr: List[int], low: int, high: int, target: int) -> int:
+    """Выполняет бинарный поиск в упорядоченном списке."""
+    if low <= high:
+        mid = low + (high - low) // 2
         if arr[mid] == target:
             return mid
-        elif arr[mid] < target:
-            return binary_search(arr, mid + 1, ub, target)
-        else:
-            return binary_search(arr, lb, mid - 1, target)
-    else:
-        return -1
-
+        if arr[mid] < target:
+            return binary_search(arr, mid + 1, high, target)
+        return binary_search(arr, low, mid - 1, target)
+    return -1
 
 if __name__ == '__main__':
     rand_num_list: List[int] = sorted([random.randint(1, 50) for _ in range(10)])
     target: int = random.randint(1, 50)
-    print("List: {}\nTarget: {}\nIndex: {}".format(
-        rand_num_list, target, binary_search(rand_num_list, 0, len(rand_num_list) - 1, target)
-    ))
+    print(f"Список: {rand_num_list}\nЦель: {target}\nИндекс: "
+          f"{binary_search(rand_num_list, 0, len(rand_num_list) - 1, target)}")
