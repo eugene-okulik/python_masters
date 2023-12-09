04daf8e5c48f6fc58b622fd6b28f0aaee5d8bb82
Dmitry Samokhin <virys2@gmail.com>
2023-12-09 18:18:27 +0300

    fix

--- homework/dmitry_samokhin/lesson_10/map_filter.py
+++ homework/dmitry_samokhin/lesson_10/map_filter.py
@@ -1,5 +1,7 @@
-temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 
-                22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+temperatures = [
+    20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 
+    22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23
+]
 
 hot_days = tuple(filter(lambda x: x > 28, temperatures))
 print(max(hot_days), min(hot_days), sum(hot_days) // len(hot_days))
