431213a67966232ceed8432d46c5fd84ebd86295
Eugeny Okulik <tiburon854@gmail.com>
2023-12-09 17:08:27 +0300

    updated formatting

--- homework/natallia_tsiushkevich/homework_2/fix_me.py
+++ homework/natallia_tsiushkevich/homework_2/fix_me.py
@@ -5,8 +5,8 @@ def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
     """A Binary Search Example which has O(log n) time complexity."""
     if lb <= ub:
         mid: int = lb + (ub - lb) // 2
-    if arr[mid] == target:
-        return mid
+        if arr[mid] == target:
+            return mid
         elif arr[mid] < target:
             return binary_search(arr, mid + 1, ub, target)
         else:
@@ -15,9 +15,16 @@ def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
         return -1
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     rand_num_li: List[int] = sorted([random.randint(1, 50) for _ in range(10)])
-target: int = random.randint(1, 50)
-print("List: {}\nTarget: {}\nIndex: {}".format(
-    rand_num_li, target, binary_search(rand_num_li, 0, len(rand_num_li) - 1, target))
-)
+    target: int = random.randint(1, 50)
+    print(
+        "List: {}\nTarget: {}\nIndex: {}".format(
+            rand_num_li, target, binary_search(
+                rand_num_li,
+                0,
+                len(rand_num_li) - 1,
+                target
+                )
+            )
+        )
