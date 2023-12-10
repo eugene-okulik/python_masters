aa5af37151dabe23c0ce2f419a0201a605bbd714
EG-QA-12 <egskypro@mail.ru>
2023-12-10 13:21:58 +0300

    hm3 done

---
+++ homework/evgeny_gusinets/homework_3/task4.py
@@ -0,0 +1,15 @@
+"""
+Calculation of the hypotenuse and area of a right-angled triangle.
+"""
+
+A = 3
+B = 4
+
+# Finding the hypotenuse using the Pythagorean theorem
+HYPOTENUSE = (A**2 + B**2)**0.5
+
+# Finding the area of a right-angled triangle
+AREA = (A * B) / 2
+
+print("Гипотенуза: ", HYPOTENUSE)
+print("Площадь: ", AREA)
