456c433ebe9f6f615448006773a007ac9f60485d
Kobzeva Alina <kobzeva_lina@mail.ru>
2024-01-02 23:03:48 +0300

    fix

--- homework/kobzeva_alina/homework_12/lesson_12(task1).py
+++ homework/kobzeva_alina/homework_12/lesson_12(task1).py
@@ -1,12 +1,11 @@
 from datetime import datetime
-
 date_str = "jan 15, 2023 - 12:05:33"
 date = datetime.strptime(date_str, "%b %d, %Y - %H:%M:%S")
-
 month_name = date.strftime("%B")
 print(month_name)
 
 formatted_date = date.strftime("%d.%m.%Y, %H:%M")
 print(formatted_date)
+
 
 
