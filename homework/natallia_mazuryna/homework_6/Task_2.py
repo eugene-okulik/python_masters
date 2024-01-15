09b69548efe565c91f934ce06b8c61f941644cdc
Natallia Mazuryna <natallia.mazuryna@gmail.com>
2024-01-15 23:53:31 +0100

    homework_6_done

---
+++ homework/natallia_mazuryna/homework_6/Task_2.py
@@ -0,0 +1,20 @@
+first_result_line = 'результат операции: 42'
+first_result_number_index = first_result_line.index('42')
+final_1st_number = first_result_line[first_result_number_index:]
+final_1st_number_int = int(final_1st_number)
+first_final_result = final_1st_number_int + 10
+print(first_final_result)
+
+second_result_line = 'результат операции: 547'
+second_result_number_index = second_result_line.index('547')
+final_2nd_number = second_result_line[second_result_number_index:]
+final_2nd_number_int = int(final_2nd_number)
+second_final_result = final_2nd_number_int + 10
+print(second_final_result)
+
+third_result_line = 'результат: 5'
+third_result_number_index = third_result_line.index('5')
+final_3rd_number = third_result_line[third_result_number_index:]
+final_3rd_number_int = int(final_3rd_number)
+third_final_result = final_3rd_number_int + 10
+print(third_final_result)
---
+++ homework/natallia_mazuryna/homework_6/Task_3.py
@@ -0,0 +1,3 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+print(f'Students {", ".join(students)} study these subjects: {", ".join(subjects)}')
