e044829212f57e5ea1622d52782fccabb3564577
Moser <moserturbo@gmail.com>
2024-01-15 21:38:30 +0100

    Sergey Molchun homework lesson 16 forward slash fix

--- homework/sergey_molchun/lesson_16/main.py
+++ homework/sergey_molchun/lesson_16/main.py
@@ -1,9 +1,8 @@
 import os.path
 
-ROOT_FOLDER_PATH = os.path.dirname(__file__).replace("\sergey_molchun\lesson_16", "")
-TARGET_PATH = f"{ROOT_FOLDER_PATH}\eugene_okulik\Lesson_16\hw"
+CURRENT_PATH = os.path.dirname(__file__).replace(r"\sergey_molchun\lesson_16", "")
+TARGET_PATH = fr"{CURRENT_PATH}\eugene_okulik\Lesson_16\hw"
 TARGET_FILE = os.path.join(TARGET_PATH, "data.log")
-print(TARGET_FILE)
 
 with open(TARGET_FILE, 'r', encoding="UTF-8") as log_file:
     elements = log_file.readlines()
