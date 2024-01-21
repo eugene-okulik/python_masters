f664ba7bb95a5b94208e0f083b71268f2a4c7963
Moser <moserturbo@gmail.com>
2024-01-21 14:49:36 +0100

    Sergey Molchun homework lesson 16 review point fix

--- homework/sergey_molchun/lesson_16/main.py
+++ homework/sergey_molchun/lesson_16/main.py
@@ -1,8 +1,23 @@
 import os.path
 
+
+"""TODO: 
+В папке homework/eugene_okulik/Lesson_16/hw лежит файл data.log
+
+Файл не копируйте и никуда не переносите. Прочитайте этот файл и 
+превратите всё его содержимое в питоновский словарь такого вида:
+
+{
+    '2022-05-18 12:19:51.685496': '2022-05-18 12:19:51.685496 ::WARNING:: Memory is almost full',
+    '2022-05-18 12:20:55.783416': '2022-05-18 12:20:55.783416 ::INFO:: Reading data from disk',
+    '2022-05-18 12:20:57.923814': '2022-05-18 12:20:57.923814 ::ERROR:: No response from disk, data can not be retrieved',
+    '2022-05-18 12:20:59.369107': '2022-05-18 12:20:59.369107 ::ERROR:: Exited abnormally'
+}
+"""
+
 CURRENT_PATH = os.path.dirname(__file__).replace(r"\sergey_molchun\lesson_16", "")
-TARGET_PATH = fr"{CURRENT_PATH}\eugene_okulik\Lesson_16\hw"
-TARGET_FILE = os.path.join(TARGET_PATH, "data.log")
+TARGET_FILE = os.path.join(CURRENT_PATH, 'eugene_okulik', 'Lesson_16', 'hw', 'data.log')
+
 
 with open(TARGET_FILE, 'r', encoding="UTF-8") as log_file:
     elements = log_file.readlines()
