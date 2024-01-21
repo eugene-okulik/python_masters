9554d4819ef93e406603faca29f8b15acbed0f0d
Eugeny Okulik <tiburon854@gmail.com>
2024-01-21 13:48:25 +0300

    Files from lesson 19

--- homework/eugene_okulik/Lesson_19/configured_db.py
+++ homework/eugene_okulik/Lesson_19/configured_db.py
@@ -1,13 +1,16 @@
 import mysql.connector as mysql
-import creds
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 
 with mysql.connect(
-    user=creds.user,
-    passwd=creds.passwd,
-    host=creds.host,
-    port=creds.port,
-    database=creds.database
+    user=os.getenv('DB_USER'),
+    passwd=os.getenv('DB_PASSW'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    database=os.getenv('DB_NAME')
 ) as db:
     cursor = db.cursor(dictionary=True)
     cursor.execute('SELECT * FROM students')
