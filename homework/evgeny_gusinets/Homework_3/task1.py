4b740f70188ea6c2e40ff03b221eeb0761b844c7
EG-QA-12 <egskypro@mail.ru>
2024-01-22 20:11:14 +0300

    update files_hw3

---
+++ homework/evgeny_gusinets/Homework_3/task1.py
@@ -0,0 +1,13 @@
+# Простые арифметические операции.
+a = 60
+b = 4
+
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+quotient_result = a / b
+
+print("Сумма:", sum_result)
+print("Разность:", difference_result)
+print("Произведение:", product_result)
+print("Деление:", quotient_result)
