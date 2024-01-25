f81284beeff19323acf8df34b17582e1abf6c33b
lzbglk123 <144231595+lzbglk123@users.noreply.github.com>
2024-01-25 20:32:24 +0100

    Updated task_1 for homework_7.py

--- homework/elizaveta_buglak/homework_7/task_1 for homework_7.py
+++ homework/elizaveta_buglak/homework_7/task_1 for homework_7.py
@@ -5,10 +5,8 @@ text_1 = text.split()
 text_final = []
 
 for word in text_1:
-    if word.endswith(','):
-        text_final.append(f"{word[:-1] + 'ing' + word[-1:]}")
-    elif word.endswith('.'):
-        text_final.append(f"{word[:-1] + 'ing' + word[-1:]}")
+    if word.endswith(',') or word.endswith('.'):
+    text_final.append(f"{word[:-1] + 'ing' + word[-1:]}")
     else:
         text_final.append(word + 'ing')
 
