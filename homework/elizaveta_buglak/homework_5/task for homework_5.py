a1ff980a33987949b66b368ea0ea9443114e8877
lzbglk123 <144231595+lzbglk123@users.noreply.github.com>
2024-01-25 20:23:53 +0100

    Updated task #5 for homework_5.py

--- homework/elizaveta_buglak/homework_5/task for homework_5.py
+++ homework/elizaveta_buglak/homework_5/task for homework_5.py
@@ -19,7 +19,7 @@ my_dict['list'].pop(1)
 # 3: Для того, что хранится под ключом ‘dict’:
 # добавьте элемент с ключом ('i am a tuple',) и любым значением
 # удалите какой-нибудь элемент
-my_dict["dict"].update({'i am a tuple': '10'})
+my_dict["dict"].update({("i am a tuple",): (10)})
 my_dict['dict'].pop('surname')
 
 # 4: Для того, что хранится под ключом ‘set’:
