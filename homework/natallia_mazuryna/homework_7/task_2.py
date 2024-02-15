e77746ccbf776154ec671c3d9e5779c8a52f9d81
Natallia Mazuryna <natallia.mazuryna@gmail.com>
2024-02-15 21:59:40 +0100

    fixes_for_homework_7

--- homework/natallia_mazuryna/homework_7/task_2.py
+++ homework/natallia_mazuryna/homework_7/task_2.py
@@ -1,6 +1,8 @@
 all_numbers = list(range(1, 101))
 for number in all_numbers:
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        print('FuzzBuzz')
+    elif number % 3 == 0:
         print('Fuzz')
     elif number % 5 == 0:
         print('Buzz')
