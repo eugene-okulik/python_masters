90ad85bdc9a300d195db218fda7b20eb557df65b
Yana Kormshchikova <veryutina.yana@yandex.ru>
2024-03-11 21:43:03 +0300

    fixed comments after rewiew

--- homework/yana_kormshchikova/lesson_13/lesson_13.py
+++ homework/yana_kormshchikova/lesson_13/lesson_13.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, author, title, pages_number, pages_material, text, isbn):
+    def __init__(self, author, title, pages_number,  isbn, text=True, pages_material='бумага'):
         self.pages_material = pages_material
         self.text = text
         self.title = title
@@ -20,11 +20,11 @@ class Book:
                   f'материал: {self.pages_material}')
 
 
-book_1 = Book('Булгаков Михаил Афанасьевич', 'Мастер и Маргарита', 512, 'бумага', True, '978-5-17-149175-8')
-book_2 = Book('Достоевский Федор Михайлович', 'Преступление и наказание', 672, 'бумага', True, '978-5-17-090630-7')
-book_3 = Book('Джейн Остин', 'Гордость и предубеждение', 400, 'бумага', True, '978-5-392-40306-6')
-book_4 = Book('Уайльд Оскар', 'Портрет Дориана Грея', 368, 'бумага', True, '978-5-389-04564-4')
-book_5 = Book('Пушкин Александр Сергеевич', 'Евгений Онегин', 448, 'бумага', True, '978-5-389-04903-1')
+book_1 = Book('Булгаков Михаил Афанасьевич', 'Мастер и Маргарита', 512, '978-5-17-149175-8')
+book_2 = Book('Достоевский Федор Михайлович', 'Преступление и наказание', 672, '978-5-17-090630-7')
+book_3 = Book('Джейн Остин', 'Гордость и предубеждение', 400, '978-5-392-40306-6')
+book_4 = Book('Уайльд Оскар', 'Портрет Дориана Грея', 368, '978-5-389-04564-4')
+book_5 = Book('Пушкин Александр Сергеевич', 'Евгений Онегин', 448, '978-5-389-04903-1')
 
 book_2.is_reserved()
 book_5.is_reserved()
@@ -38,8 +38,8 @@ book_5.book_info()
 
 class SchoolBook(Book):
 
-    def __init__(self, author, title, pages_number, pages_material, text, isbn, subject, school_class):
-        super().__init__(author, title, pages_number, pages_material, text, isbn)
+    def __init__(self, author, title, pages_number, isbn, subject, school_class, text=True, pages_material='бумага'):
+        super().__init__(author, title, pages_number, isbn, text=True, pages_material='бумага')
         self.subject = subject
         self.school_class = school_class
         self.has_tasks = False
@@ -56,11 +56,11 @@ class SchoolBook(Book):
                   f'предмет: {self.subject}, класс: {self.school_class}')
 
 
-school_book_1 = SchoolBook('Поляков, Мерзляк', 'Алгебра и начала математического анализа', 480, 'бумага', True,
+school_book_1 = SchoolBook('Поляков, Мерзляк', 'Алгебра и начала математического анализа', 480,
                            '978-5-360-12203-6', 'Алгебра', '10')
-school_book_2 = SchoolBook('И. Л. Андреев ', 'История России. Конец XVII-XVIII век', 269, 'бумага', True,
+school_book_2 = SchoolBook('И. Л. Андреев ', 'История России. Конец XVII-XVIII век', 269,
                            '9785090878456', 'История', '8')
-school_book_3 = SchoolBook('Пасечник В. В., Каменский А. А.', 'Биология', 208, 'бумага', True,
+school_book_3 = SchoolBook('Пасечник В. В., Каменский А. А.', 'Биология', 208,
                            '978-5-09-088223-1', 'Биология', '9')
 
 school_book_1.is_reserved()
