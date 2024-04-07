bf7e4aa9f4101ad977bd28a1a35e0d3a588992f0
Eugeny Okulik <tiburon854@gmail.com>
2024-04-07 12:22:39 +0300

    UI tests - headless

--- ui_tests_eokulik/conftest.py
+++ ui_tests_eokulik/conftest.py
@@ -1,12 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from ui_tests_eokulik.pages.whats_new_page import WhatsNew
 from time import sleep
 
 
 @pytest.fixture()
 def driver():
-    chrome = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    chrome = webdriver.Chrome(options=options)
     chrome.implicitly_wait(5)
     sleep(3)  # Чтобы перетягивать окошко
     chrome.maximize_window()
